90d3ac8210bc36ec05da0677fb1dcef508f9b815
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2023-08-04 00:54:06 +0600

    Create post.ts

---
+++ src/post.ts
@@ -0,0 +1,71 @@
+import * as core from '@actions/core'
+import {getExecOutput} from '@actions/exec'
+import * as fs from 'fs'
+import {PipelineEnforcerEndFlags} from './types'
+import {extractEndInputs, validateEndInputs} from './inputs'
+
+class CommandError extends Error {
+  exitCode: number
+
+  constructor(exitCode: number, message: string) {
+    super(message)
+    this.exitCode = exitCode
+  }
+}
+
+const executePipelineEnforcerEnd = async (flags: PipelineEnforcerEndFlags) => {
+  const {khulnasoftKey, khulnasoftSecret, verbose} = flags
+  if (!fs.existsSync('./pipeline-enforcer')) {
+    throw new Error('pipeline-enforcer was not found')
+  }
+
+  const pipelineEnforcerCommand = `./pipeline-enforcer ci end ${
+    verbose ? '-v' : ''
+  }`
+
+  const result = await getExecOutput(pipelineEnforcerCommand, [], {
+    ignoreReturnCode: true,
+    env: {
+      ...process.env,
+      AQUA_KEY: khulnasoftKey,
+      AQUA_SECRET: khulnasoftSecret
+    }
+  })
+
+  if (result.exitCode != 0) {
+    throw new CommandError(result.exitCode, result.stdout + result.stderr)
+  }
+}
+
+async function run(): Promise<void> {
+  const flags = extractEndInputs()
+  try {
+    validateEndInputs(flags)
+
+    core.info('Ending pipeline-enforcer run')
+    await executePipelineEnforcerEnd(flags)
+    core.debug('pipeline-enforcer ended successfully')
+  } catch (error) {
+    if (error instanceof CommandError) {
+      if (error.exitCode == 13) {
+        core.setFailed(
+          'KhulnaSoft Security Pipeline Enforcer - assurance policies failed'
+        )
+      } else {
+        core.setFailed(error.message)
+      }
+      process.exitCode = error.exitCode
+    } else if (error instanceof Error) {
+      core.setFailed(error.message)
+    }
+  } finally {
+    const {logFile} = flags
+    if (logFile && fs.existsSync(logFile)) {
+      const log = fs.readFileSync(logFile, 'utf8')
+      core.info(`pipeline-enforcer logs`)
+      core.info(log)
+    }
+  }
+}
+
+run()
